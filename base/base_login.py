3f47083e798175774ca91ce153aa0e252f1d7ebe
MgLay123 <50288947+MgLay123@users.noreply.github.com>
2019-07-27 23:55:16 +0630

    Update base_login.py

--- base/base_login.py
+++ base/base_login.py
@@ -3,13 +3,4 @@ from selenium.webdriver.support.wait import WebDriverWait
 
 from base.base_driver import init_driver
 from page.page import Page
-from base.base_data import *
-
-
-def login():
-    driver = init_driver()
-    page = Page(driver)
-    page.login_page.input_phoneNO(phoneNo)
-    page.login_page.input_smscode(smscode)
-    page.login_page.click_start()
-    return page, driver
+from base.base_data import 
